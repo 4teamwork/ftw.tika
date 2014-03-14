de4633f2dcf7acb13efcd94ffb9f28be9da682b3
Jonas Baumann <jone@jone.ch>
2014-03-14 10:48:42 +0100

    Testing: move convert_asset helper to helper module.

--- ftw/tika/tests/test_integration.py
+++ ftw/tika/tests/test_integration.py
@@ -1,25 +1,8 @@
-from Products.CMFCore.utils import getToolByName
 from ftw.tika.testing import FTW_TIKA_INTEGRATION_TESTING
 from ftw.tika.testing import TIKA_SERVER_INTEGRATION_TESTING
+from ftw.tika.tests.helpers import convert_asset
 from unittest2 import TestCase
-from zope.component.hooks import getSite
-import os.path
 
-
-ASSETS = os.path.join(os.path.dirname(__file__), 'assets')
-OFFICE_MIME = 'application/vnd.openxmlformats-officedocument.'
-MIMETYPES_BY_EXTENSION = {
-    '.docx': '%swordprocessingml.document' % OFFICE_MIME}
-
-
-def convert_asset(filename):
-    path = os.path.join(ASSETS, filename)
-    transforms = getToolByName(getSite(), 'portal_transforms')
-    _, extension = os.path.splitext(filename)
-    mimetype = MIMETYPES_BY_EXTENSION[extension]
-    with open(path, 'r') as asset:
-        stream = transforms.convertTo('text/plain', asset, mimetype=mimetype)
-        return stream.getData().strip()
 
 
 class TestConversion(TestCase):
