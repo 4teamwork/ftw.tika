9fcb911ce363cf42866cd3a8b7a7a74e1a68d6ce
Lukas Graf <lukas.graf@4teamwork.ch>
2015-10-25 16:56:44 +0100

    Factor out detection of password protected docs upon
    conversion failure.

---
+++ ftw/tika/protected_docs.py
@@ -0,0 +1,31 @@
+"""Utility functions for detecting password protected documents upon
+conversion failure.
+"""
+
+from ftw.tika import mimetypes
+
+
+PROTECTED_PDF_MSGS = (
+    # Tika 1.5, 1.6
+    'Error: The supplied password does not match either the'
+    ' owner or user password in the document.',
+)
+
+PROTECTED_MSOFFICE_MSGS = (
+    # Tika 1.5, 1.6
+    'org.apache.tika.exception.EncryptedDocumentException',
+)
+
+
+def is_protected_doc(exc, mimetype):
+    """Figure out whether conversion failed because we're dealing with
+    a password protected document, based on the document's MIME type and
+    the Java exception.
+    """
+    if mimetype in mimetypes.PDF_TYPES:
+        return any(msg in str(exc) for msg in PROTECTED_PDF_MSGS)
+
+    if mimetype in mimetypes.MS_OFFICE_TYPES:
+        return any(msg in str(exc) for msg in PROTECTED_MSOFFICE_MSGS)
+
+    return False
